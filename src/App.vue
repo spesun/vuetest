8824e2a1757bb410c658e6dbdd9b6f5ffcde27e7
sunww <sunww@asiainfo.com>
2019-04-19 15:04:30 +0800

    修改的上传

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-link to=/foo>aa</router-link>
+    <router-link to=/foo></router-link>
     <router-view></router-view>
   </div>
 </template>
