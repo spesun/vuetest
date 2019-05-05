af7a3d06e5cb916a57d16e2899e830568b40d616
sunww <sunweiwei@guiji.ai>
2019-05-05 09:40:26 +0800

    测试、注释增加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-link to=/foo></router-link>
+    <router-link to=/foo>测试foo路由</router-link>
+    <router-link to=/>测试根路由</router-link> <!--默认路由就是根路由-->
     <router-view></router-view>
   </div>
 </template>
